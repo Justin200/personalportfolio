70beff37f25ce204568912154c94fe698e0e68cc
Justin Hu <jxh1075@case.edu>
2020-05-27 17:53:19 -0700

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import NavBar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 import './App.css';
 
 class App extends React.Component {
@@ -29,7 +32,24 @@ class App extends React.Component {
   render(){
     return (
       <Router>
-        
+        <Container className="p-0" fluid={true}>
+
+
+          <NavBar className="border-bottom" bg="transparent" expand="lg"> {/*Responsive collapsing set to large*/}
+            <NavBar.Brand>Justin Hu</NavBar.Brand>
+
+            <NavBar.Toggle aria-controls="navbar-toggle" /> 
+            <NavBar.Collapse id="navbar-toggle"> {/* Dropdown navigation bar for mobile*/}
+              <Nav className="ml-auto"> {/*left margin*/}
+                <Link className="nav-link" to="/">Home</Link>
+                <Link className="nav-link" to="/about">About</Link>
+                <Link className="nav-link" to="/contact">Contact</Link>
+              </Nav>
+            </NavBar.Collapse>  
+          </NavBar>
+
+
+        </Container>
       </Router>
     );
   }
